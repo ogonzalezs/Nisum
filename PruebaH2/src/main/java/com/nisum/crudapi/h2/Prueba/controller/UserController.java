b0d9c3a8cf743776bb8d629923a3b5ee2e923362
Orlando J Gonzalez S <ogonzalezs@gmail.com>
2024-10-06 15:35:19 -0300

    Limpieza codigo

--- PruebaH2/src/main/java/com/nisum/crudapi/h2/Prueba/controller/UserController.java
+++ PruebaH2/src/main/java/com/nisum/crudapi/h2/Prueba/controller/UserController.java
@@ -4,6 +4,11 @@ import com.nisum.crudapi.h2.Prueba.dto.UserRequest;
 import com.nisum.crudapi.h2.Prueba.dto.UserResponse;
 import com.nisum.crudapi.h2.Prueba.services.UserService;
 import com.nisum.crudapi.h2.Prueba.entities.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +25,15 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Lista todos los usuarios")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuarios listados exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",
+                    content = @Content) })
     @GetMapping
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers();
@@ -30,6 +44,15 @@ public class UserController {
         return userService.getUserById(id);
     }
 
+    @Operation(summary = "Crear un nuevo usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario creado exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",
+                    content = @Content) })
     @PostMapping
     public UserResponse createUser(@RequestBody UserRequest userRequest) {
         return userService.createUser(userRequest);
@@ -40,11 +63,29 @@ public class UserController {
         return userService.updateUser(id, userRequest);
     }
 
+    @Operation(summary = "Borra un usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario borrado exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",
+                    content = @Content) })
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable UUID id) {
         userService.deleteUser(id);
     }
 
+    @Operation(summary = "Muestra la informacion de un usuario a partir del email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",
+                    content = @Content) })
     @GetMapping("/email")
     public UserResponse getUserByEmail(@RequestBody UserRequest request) {
         return userService.getUserByEmail(request);
